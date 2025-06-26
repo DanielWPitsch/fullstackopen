e17dbe6880bc5df0fe1c92596e82082a8755b1f6
Daniel Warella Pitsch <daniel.pitsch@SERPB.local>
2025-06-26 11:55:11 -0300

    part1 javascript done

--- part1/fopenapp/README.md
+++ part1/fopenapp/README.md
@@ -1,18 +1,15 @@
 # Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+npx create-react-app fopenapp
 
 ## Available Scripts
 
 In the project directory, you can run:
 
-### `npm start`
+### `npm start -- --reset-cache`
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
 
 ### `npm test`
 
--- part1/fopenapp/src/App.js
+++ part1/fopenapp/src/App.js
@@ -6,42 +6,45 @@ const Part = ({ name, exercises }) => {
   return <p>{name} {exercises}</p>
 }
 
-const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
+const Content = ({ parts }) => {
   return (
     <div>
-      <Part name={part1} exercises={exercises1} />
-      <Part name={part2} exercises={exercises2} />
-      <Part name={part3} exercises={exercises3} />
+      {parts.map((part, index) => (
+        <Part key={index} name={part.name} exercises={part.exercises} />
+      ))}
     </div>
   )
 }
 
-const Total = ({ exercises1, exercises2, exercises3 }) => {
-  return <p>Total number of exercises: {exercises1 + exercises2 + exercises3}</p>
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum +part.exercises, 0)
+  return <p>Total number of exercises: {total}</p>
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Desenvolvimento de aplicação Half Stack',
+    parts: [
+      {
+      name: 'Fundamentos da biblioteca React',
+      exercises: 10
+      },
+      {
+        name: 'Usando props para passar dados',
+        exercises: 7
+      },
+      {
+        name: 'Estado de um componente',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content
-        part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}
-      />
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
