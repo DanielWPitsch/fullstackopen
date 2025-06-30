df1c8bbfe1e21a1ab224333170220d55bb368535
Daniel Warella Pitsch <daniel.pitsch@SERPB.local>
2025-06-30 11:49:00 -0300

    2.1 done

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -21,32 +21,40 @@ const Total = ({ parts }) => {
   return <p>Total number of exercises: {total}</p>
 }
 
-const App = () => {
-  const course = {
-    name: 'Desenvolvimento de aplicação Half Stack',
-    parts: [
-      {
-      name: 'Fundamentos da biblioteca React',
-      exercises: 10
-      },
-      {
-        name: 'Usando props para passar dados',
-        exercises: 7
-      },
-      {
-        name: 'Estado de um componente',
-        exercises: 14
-      }
-    ]
-  }
-
+const Course = ({ course }) => {
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
     </div>
-  )
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+      name: 'Fundamentals of React',
+      exercises: 10,
+      id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
