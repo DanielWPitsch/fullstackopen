3d0665ff2f8cd04397f2c3835258faf172ac78db
Daniel Warella Pitsch <daniel.pitsch@SERPB.local>
2025-06-30 12:09:54 -0300

    2.5 done

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,36 +1,4 @@
-const Header = ({ course }) => {
-  return <h1>{course}</h1>
-}
-
-const Part = ({ name, exercises }) => {
-  return <p>{name} {exercises}</p>
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part, index) => (
-        <Part key={index} name={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  )
-}
-
-const Total = ({ parts }) => {
-  //already did it
-  const total = parts.reduce((sum, part) => sum +part.exercises, 0)
-  return <p><strong> Total of {total} exercises </strong></p>
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
+import Course from "./Course";
 
 const App = () => {
   const courses = [
